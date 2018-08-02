0e47367695951dae6b11c158ce66a16c2a3e0cc5
xzx <xuzhenxing@canaan-creative.com>
2018-08-02 17:44:41 +0800

    Update slt-options.conf -> config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+config = {
+    'time': 60,
+    'ip': '192.168.1.249',
+    'options': {
+            '--avalon8-freq 0:0:0:0:0:0:600  --avalon8-voltage-level 6',
+            '--avalon8-freq 0:0:0:0:0:0:700  --avalon8-voltage-level 6',
+            '--avalon8-freq 0:0:0:0:0:0:800  --avalon8-voltage-level 6',
+        }
+}
