822c4e2456626a6d2bc356294c6d27e69688366e
xzx <xuzhenxing@canaan-creative.com>
2018-08-06 20:38:56 +0800

    Update debuglog.py

--- debuglog.py
+++ debuglog.py
@@ -8,9 +8,47 @@ import os
 import logging
 import remote
 import time
+import re
 
 # Config logging level
 logging.basicConfig(level=logging.DEBUG)
+
+_patterns = {
+    'ghsmm': "GHSmm\[[0-9]{1,6}\.[0-9]{1,2}\]",
+    'temp': "Temp\[[0-9]{1,6}\]",
+    'tmax': "TMax\[[0-9]{1,6}\]",
+    'wu': "WU\[[0-9]{1,6}\.[0-9]{1,2}\]",
+    'dh': "DH\[[0-9]{1,3}\.[0-9]{1,3}\%\]",
+    'power': "Power\[[0-9]{1,3}\.[0-9]{1,3}\]",
+    'iout': "Iout\[[0-9]{1,3}\.[0-9]{1,3}\]",
+}
+
+_patternd = {
+    'ghsmm': "[0-9]{1,6}\.[0-9]{1,2}",
+    'temp': "[0-9]{1,6}",
+    'tmax': "[0-9]{1,6}",
+    'wu': "[0-9]{1,6}\.[0-9]{1,2}",
+    'dh': "[0-9]{1,3}\.[0-9]{1,3}",
+    'power': "[0-9]{1,3}\.[0-9]{1,3}",
+    'iout': "[0-9]{1,3}\.[0-9]{1,3}",
+}
+
+def read_debuglog(opt):
+    with open('debug.log', 'r') as f:
+        for lines in f:
+            tmp = str(re.findall(_patternd[opt], str(re.findall(_patterns[opt], lines)))).strip("[']")
+            with open(opt + '.log', 'a') as f:
+                f.write(tmp + '\n')
+
+def gen_ghsav():
+    with open('wu.log', 'r') as f:
+        for line in f:
+            tmp = round(float(line.strip()) / 60 * 2**32 / 10**9, 3)
+            with open('ghsav.log', 'a') as f:
+                f.write(str(tmp) + '\n')
+
+def power_rate():
+    os.system("paste $i.GHSav $i.Power | awk '{printf ('%.3f\n', ($2 / $1))}' > ph.log")
 
 def debuglog_files(ip_dirs):
     datas = remote.remote_cmd(ip, 0)
@@ -47,31 +85,24 @@ if __name__ == '__main__':
     freq = sys.argv[2]
     volt_level = sys.argv[3]
 
+    '''
     ip_dirs = "result" + "-" + ip
     logging.debug("debuglog create dirs: %s", ip_dirs)
     os.makedirs(ip_dirs)
 
     debuglog_files(ip_dirs)
     handle_debuglog(ip_dirs, ip, freq, volt_level)
+    '''
 
-'''
-    cat $i | sed 's/] /\]\n/g' | grep GHSmm | sed 's/GHSmm\[//g' | sed 's/\]//g' > $i.GHSmm
-    cat $i | sed 's/] /\]\n/g' | grep Temp  | sed 's/Temp\[//g'  | sed 's/\]//g' > $i.Temp
-    cat $i | sed 's/] /\]\n/g' | grep TMax  | sed 's/TMax\[//g'  | sed 's/\]//g' > $i.TMax
-    cat $i | sed 's/] /\]\n/g' | grep WU    | sed 's/WU\[//g'    | sed 's/\]//g' > $i.WU
-    cat $i | sed 's/] /\]\n/g' | grep DH    | sed 's/DH\[//g'    | sed 's/\]//g' > $i.DH
-    cat $i | sed 's/] /\]\n/g' | grep Power | sed 's/Power\[//g' | sed 's/\]//g' > $i.Power
-    cat $i | sed 's/] /\]\n/g' | grep "Iout\["    | sed 's/Iout\[//g'    | sed 's/\]//g' > $i.Iout
-    cat $i | sed 's/] /\]\n/g' | grep V0 | awk '{ print $3 }' > $i.V0
+    read_debuglog('ghsmm')
+    read_debuglog('temp')
+    read_debuglog('tmax')
+    read_debuglog('wu')
+    read_debuglog('dh')
+    read_debuglog('power')
+    read_debuglog('iout')
+    gen_ghsav()
+    power_rate()
 
-    # According to WU value, calculate GHSav.
-    # Formula: ghsav = WU / 60 * 2^32 / 10^9
-    cat $i.WU | awk '{printf ("%.2f\n", ($1/60*2^32/10^9))}' > $i.GHSav
-
-    # Power / GHSav
-    paste $i.GHSav $i.Power | awk '{printf ("%.3f\n", ($2/$1))}' > ph.log
-
-    Result=Results_$dirname
-
-    paste -d, freq.log voltage.log $i.GHSmm $i.Temp $i.TMax $i.WU $i.GHSav $i.DH $i.Iout $i.V0 $i.Power ph.log options.log > ${Result#.log}.csv
-'''
+    os.system("paste -d, ghsmm.log temp.log tmax.log wu.log dh.log power.log iout.log ghsav.log >> result.csv")
+    os.system("rm ghsmm.log temp.log tmax.log wu.log dh.log power.log iout.log ghsav.log")
