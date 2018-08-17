7b3f74b57f16ebb7073c3067627e849dfb5eb921
xzx <xuzhenxing@canaan-creative.com>
2018-08-17 10:17:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@
 config = {
     'time': 60,
 
-    'ip': {
+    'ip': [
         '192.168.1.231',
         '192.168.1.233',
         '192.168.1.240',
-    },
+        ],
 
-    'options': {
+    'options': [
         '--avalon8-freq 0:0:0:0:0:0:600 --avalon8-voltage-level 10 --avalon8-freq-sel 6 --avalon8-smart-speed 0',
         '--avalon8-freq 0:0:0:0:0:0:650 --avalon8-voltage-level 10 --avalon8-freq-sel 6 --avalon8-smart-speed 0',
         '--avalon8-freq 0:0:0:0:0:0:625 --avalon8-voltage-level 10 --avalon8-freq-sel 6 --avalon8-smart-speed 0',
+        ]
     }
-}
